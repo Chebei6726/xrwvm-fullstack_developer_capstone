1e2f58270da8f1121d1f4039c460c80cb0f80b48
Gideon Kipleting <chebei92@gmail.com>
2024-09-19 00:14:05 +0300

    changes of React pages

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,12 +1,12 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+    'backend_url', default="http://127.0.0.1:3030")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
@@ -21,7 +21,7 @@ def get_request(endpoint, **kwargs):
 
     request_url = backend_url+endpoint+"?"+params
 
-    print("GET from {} ".format(request_url))
+    print("GET from {}".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
